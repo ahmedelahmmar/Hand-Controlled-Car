efd896e319ecee5a0c587b4a2937e3715de44416
Ahmed Alaa <111080073+ahmedelahmmar@users.noreply.github.com>
2022-10-30 21:03:39 +0200

    Final

--- Glove's Kit/Sources/5. Project/Glove_Sender/Sources/glove_sender.c
+++ Glove's Kit/Sources/5. Project/Glove_Sender/Sources/glove_sender.c
@@ -8,21 +8,21 @@
 #include "All_includes.h"
 
 #define min_unbent 800
-	#define max_unbent 1000
-	#define min_bent 120
-	#define max_bent 600
+#define max_unbent 1000
+#define min_bent 120
+#define max_bent 600
 
-    #define min_no_motion 800
-	#define max_no_motion 1000
-	#define min_low_speed 600
-	#define max_low_speed 300
-    #define min_full_speed 299
-    #define max_full_speed 120
+#define min_no_motion 800
+#define max_no_motion 1000
+#define min_low_speed 600
+#define max_low_speed 200
+#define min_full_speed 299
+#define max_full_speed 120
 
 
-	#define thumb ADC_ADC0
-	#define index_finger ADC_ADC1
-	#define middle_finger ADC_ADC2
+#define thumb ADC_u16ReadSteps(ADC_ADC4)
+#define index_finger ADC_u16ReadSteps(ADC_ADC5)
+#define middle_finger ADC_u16ReadSteps(ADC_ADC6)
 
 void ReadySteadyGo(void)
 {
@@ -31,6 +31,7 @@ void ReadySteadyGo(void)
 	//LCD_voidDisplayStr("ADC STEPS: ");
 	LCD_voidDisplayStr("HI!!!");
 	_delay_ms(4000);
+	LCD_voidCMD(_LCD_CLEAR);
 	LCD_voidMatrix(0,0);
 	LCD_voidDisplayStr("READY");
 	_delay_ms(400);
@@ -44,16 +45,17 @@ void ReadySteadyGo(void)
 	LCD_voidDisplayStr(".");
 	_delay_ms(6000);
 
+	LCD_voidCMD(_LCD_CLEAR);
 	LCD_voidMatrix(0,0);
 	LCD_voidDisplayStr("STEADY");
-	_delay_ms(400);
-	LCD_voidMatrix(0,5);
-	LCD_voidDisplayStr(".");
 	_delay_ms(400);
 	LCD_voidMatrix(0,6);
 	LCD_voidDisplayStr(".");
 	_delay_ms(400);
 	LCD_voidMatrix(0,7);
+	LCD_voidDisplayStr(".");
+	_delay_ms(400);
+	LCD_voidMatrix(0,8);
 	LCD_voidDisplayStr(".");
 	_delay_ms(6000);
 
@@ -106,16 +108,16 @@ boolean Thumb_IsBent(){
 
 boolean MiddleFinger_IsBent(){
 	if(middle_finger>=min_bent && middle_finger<=max_bent){
-			return 1;
-		}
-		else return 0;
+		return 1;
+	}
+	else return 0;
 }
 
 boolean IndexFinger_IsBent(){
 	if(index_finger>=min_bent && index_finger<=max_bent){
-				return 1;
-			}
-			else return 0;
+		return 1;
+	}
+	else return 0;
 }
 
 boolean Thumb_IsLowSpeed(){
@@ -132,42 +134,51 @@ boolean Thumb_IsFullSpeed(){
 	else return 0;
 }
 
-void Execute_glove_sender(){
-if(Thumb_IsBent()){
-	if(MiddleFinger_IsBent()){
-		//steer right
-		CarSteerRight_Send();
-	}
-	else if(IndexFinger_IsBent()){
-		//steer left
-		CarSteerLeft_Send();
-	}
-	else {
-		if(Thumb_IsLowSpeed()){
-			//move forward low speed
-			CarForward_Send();
+void Execute_glove_sender()
+{
+	if(Thumb_IsBent()){
+		if(MiddleFinger_IsBent())
+		{
+			//steer right
+			CarSteerRight_Send();
+			return;
 		}
-		else if(Thumb_IsFullSpeed()){
-			//move forward full speed
-			CarForwardFullSpeed_Send();
+		else if(IndexFinger_IsBent()){
+			//steer left
+			CarSteerLeft_Send();
+			return;
+		}
+		else {
+			if(Thumb_IsLowSpeed()){
+				//move forward low speed
+				CarForward_Send();
+				return;
+			}
+			else if(Thumb_IsFullSpeed()){
+				//move forward full speed
+				CarForwardFullSpeed_Send();
+				return;
+			}
 		}
 	}
-}
 
-else if(IndexFinger_IsBent()){
-	if(MiddleFinger_IsBent()){
-		//lights
-		LED_voidFLASH(LED0);
-		LED_voidFLASH(LED1);
-		LED_voidFLASH(LED2);
+	else if(IndexFinger_IsBent()){
+		if(MiddleFinger_IsBent()){
+			//lights
+			LED_voidFLASH(LED0);
+			LED_voidFLASH(LED1);
+			LED_voidFLASH(LED2);
+			return;
+		}
+		else{
+			//backward
+			CarBackward_Send();
+			return;
+		}
 	}
-	else{
-		//backward
-		CarBackward_Send();
+	else if(MiddleFinger_IsBent() && (!Thumb_IsBent() && !IndexFinger_IsBent())){
+		//break
+		CarStop_Send();
+		return;
 	}
-}
-else if(MiddleFinger_IsBent() && (!Thumb_IsBent() && !IndexFinger_IsBent())){
-	//break
-	CarStop_Send();
-}
 }
